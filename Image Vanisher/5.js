5d9cc45f82ba6043d93c31c0b0b071744d2e27e8
Manjeetd7 <155341789+Manjeetd7@users.noreply.github.com>
2024-03-28 17:53:59 +0530

    UP in 5.js

--- Image Vanisher/5.js
+++ Image Vanisher/5.js
@@ -2,6 +2,8 @@
     //     alert("owl clicked")
     // }
 
+//clicking on the elements and an alert pops "owl clicked " in the document
+
      // attachEvent()
     // jQuery - on
 
@@ -10,9 +12,18 @@
     // clientX, clientY, screenX, screenY
     // altkey, ctrlkey, shiftkey, keyCode
 
+//the attributes mentioned above represents when event iS Printed in console eg addEventListener('click', function(e){
+        // console.log(e);
+
     // document.getElementById('images').addEventListener('click', function(e){
     //     console.log("clicked inside the ul");
     // }, false)
+
+// this false represents event propagation. Even if we dont use it, the effect remains the same
+
+   // false comes under bubbling, as it prints what is clicked first and then whole scene
+   
+  //true comesunder capturing, which says,print the outer first the the clicked one 
 
     // document.getElementById('owl').addEventListener('click', function(e){
     //     console.log("owl clicked");
@@ -24,7 +35,12 @@
     //     e.stopPropagation()
     //     console.log("google clicked");
     // }, false)
-   
+
+  //preventDefault - STOPS THE DEFAULT FUNCTIONING OF EVENT
+
+
+    //stopPropagation - It prevents the event from bubbling up or capturing down further in the DOM hierarchy.
+
     
     document.querySelector('#images').addEventListener('click', function(e){
         console.log(e.target.tagName);
